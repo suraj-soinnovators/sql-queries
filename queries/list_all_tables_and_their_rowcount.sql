c12255807aca7214ae32bc4a240688e80b2a2fad
suraj-soinnovators <62230661+suraj-soinnovators@users.noreply.github.com>
2020-03-16 14:11:00 +0900

    basic setup

---
+++ queries/list_all_tables_and_their_rowcount.sql
@@ -0,0 +1,6 @@
+SELECT t.name, s.row_count from sys.tables t
+JOIN sys.dm_db_partition_stats s
+ON t.object_id = s.object_id
+AND t.type_desc = 'USER_TABLE'
+AND t.name not like '%dss%'
+AND s.index_id IN (0,1)
